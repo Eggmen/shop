ef186e46eef160f1195527ef8892b536b9ca34d6
Andy Karpov <andy.karpov@gmail.com>
2015-04-23 14:07:29 +0300

    Добавлена логика для допиливания показа вкладки опкций тольок для перечисляемых типов

--- scripts/FeatureForm.js
+++ scripts/FeatureForm.js
@@ -1,13 +1,16 @@
 ScriptLoader.load('Form');
 var FeatureForm = new Class({
     Extends: Form,
+
     initialize: function (el) {
         this.parent(el);
         this.prepareFilterProperties();
         this.componentElement.getElementById('feature_is_filter').addEvent('change', this.prepareFilterProperties.bind(this));
+		this.componentElement.getElementById('feature_type').addEvent('change', this.prepareFilterProperties.bind(this));
     },
+
     prepareFilterProperties: function () {
-        var filter;
+        var filter, feature_type;
 
         if ((filter = this.componentElement.getElementById('feature_is_filter')) && filter.checked) {
             this.componentElement.getElementById('feature_sysname').getParent('.field').show();
@@ -18,5 +21,14 @@ var FeatureForm = new Class({
             this.componentElement.getElementById('feature_filter_type').getParent('.field').hide();
         }
 
+		if (feature_type = this.componentElement.getElementById('feature_type')) {
+			if (['OPTION','MULTIOPTION','VARIANT'].indexOf(feature_type.get('value')) >= 0) {
+				// показываем вкладку Перечисляемые значения
+				//console.log('show options for ' + feature_type.get('value'));
+			} else {
+				// прячем вкладку Перечисляемые значения
+				//console.log('hide options for ' + feature_type.get('value'));
+			}
+		}
     }
 });
