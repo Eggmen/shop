7453065e175957f42b97803669d51f1f6e9ef337
toxaestel <toxaestel@gmail.com>
2015-04-29 15:45:22 +0300

    goods gallery

--- scripts/ProductView.js
+++ scripts/ProductView.js
@@ -4,16 +4,14 @@ var ProductView = new Class({
     initialize: function (el) {
         this.element = $(el);
 
-	    var galleryLarge = new Carousel(document.getElementById('goodsGalleryLarge'),
-		    {carousel:{
-			    controls:{
-				    styles:{
-					    forward:{
-						    marginLeft:''
-					    }
-				    }
-			    }
-		    }});
+	    var galleryLarge = new Carousel(document.getElementById('goodsGalleryLarge'));
+	    var gallerySmall = new Carousel(document.getElementById('goodsGallerySmall'), {
+		    carousel: {
+			    NVisibleItems: 3,
+			    scrollStep: 1
+		    }
+	    });
+	    var carCon = new CarouselConnector([galleryLarge, gallerySmall]);
     }
 });
 
