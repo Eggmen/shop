928460730f3a95c9cd952e4fc1a78f5b99be7817
Pavel Dubenko <d.pavka@gmail.com>
2015-05-06 16:29:44 +0300

    range

--- scripts/ProductFilter.js
+++ scripts/ProductFilter.js
@@ -1,7 +1,26 @@
-ScriptLoader.load('http://code.jquery.com/ui/1.11.4/jquery-ui.js');
-Asset.css('http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css');
-var ProductFilter = function (el) {
-    jQuery('.range', $(el)).each(function(idx, range){
-        jQuery(range).slider();
-    });
-}
+ScriptLoader.load('scripts/jquery.nouislider.all.js');
+Asset.css('jquery.nouislider.min.css');
+var ProductFilter;
+(function ($, window, document) {
+
+    ProductFilter = function (el) {
+
+        $('.range', document.id(el)).each(function (idx, el) {
+            var el = $(el).prop('slide', null);
+
+            el.noUiSlider({
+                start: [el.data('start') || 0, el.data('end') || 0],
+                connect: true,
+                step: el.data('step'),
+                range: {
+                    'min': [el.data('min') || 0],
+                    'max': [el.data('max') || 0]
+                }
+            });
+
+            el.Link('lower').to(jQuery('.lower', el));
+            el.Link('upper').to(jQuery('.upper', el));
+        });
+
+    }
+}(window.jQuery, window, document));
--- scripts/ProductView.js
+++ scripts/ProductView.js
@@ -1,23 +1,21 @@
 ScriptLoader.load('http://cdn.jsdelivr.net/jquery.slick/1.5.0/slick.min.js');
-Asset.css('http://cdn.jsdelivr.net/jquery.slick/1.5.0/slick.css');
-Asset.css('http://cdn.jsdelivr.net/jquery.slick/1.5.0/slick-theme.css');
-var ProductView = function (el) {
-    jQuery('.single-item', $(el)).slick({
-        asNavFor: '.multiple-items',
-        fade: true
-    });
-    jQuery('.multiple-items').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.single-item',
-        centerMode: true,
-        centerPadding: '25%',
-        focusOnSelect: true
-    });
-
-}
-
-
+var ProductView;
+(function ($, window, document) {
+    ProductView = function (el) {
+        el = document.id(el)
+        $('.single-item', el).slick({
+            asNavFor: '.multiple-items'
+        });
+        $('.multiple-items', el).slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.single-item',
+            /*centerMode: true,
+             centerPadding: '25%',*/
+            focusOnSelect: true
+        });
+    }
+}(window.jQuery, window, document));
 
 
 
