61f0fd69f9726a984f5e8381f80d93038b5ccc8e
Pavel Dubenko <d.pavka@gmail.com>
2015-05-11 18:41:06 +0300

    added site

--- components/Wishlist.php
+++ components/Wishlist.php
@@ -23,7 +23,7 @@ class Wishlist extends DBDataSet {
     public function __construct($name, $module, array $params = NULL) {
         parent::__construct($name, $module, $params);
         $this->setTableName('shop_wishlist');
-        $this->setFilter(['u_id' => $this->document->getUser()->getID()]);
+        $this->setFilter(['site_id' => E()->getSiteManager()->getCurrentSite()->id,'u_id' => $this->document->getUser()->getID()]);
         $this->setOrder(['w_date' => QAL::ASC]);
     }
 
@@ -40,7 +40,7 @@ class Wishlist extends DBDataSet {
         E()->getController()->getTransformer()->setFileName('../../../../core/modules/shop/transformers/single_wishlist.xslt');
         $this->setBuilder(new EmptyBuilder());
         if ($this->document->getUser()->isAuthenticated() && $this->dbh->getScalar('shop_goods', 'goods_id', ['goods_id' => $productID])) {
-            $this->dbh->modify(QAL::INSERT_IGNORE, $this->getTableName(), ['u_id' => $this->document->getUser()->getID(), 'goods_id' => $productID]);
+            $this->dbh->modify(QAL::INSERT_IGNORE, $this->getTableName(), ['site_id'=>E()->getSiteManager()->getCurrentSite()->id, 'w_date' => date('Y-m-d H:i:s'), 'u_id' => $this->document->getUser()->getID(), 'goods_id' => $productID]);
 
             $this->setProperty('count', $this->getCount());
         }
