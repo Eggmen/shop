dc641f82ef25ac0c771968d4556bff2e9d39a2ed
Andy Karpov <andy.karpov@gmail.com>
2015-05-29 16:59:31 +0300

    Добавлена функциональность учета последнего просмотренного товара / вывода списка промотренных товаров

---
+++ components/GoodsLastSeenList.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Energine\shop\components;
+
+use Energine\share\components\DataSet;
+use Energine\share\gears\ComponentProxyBuilder;
+use Energine\share\gears\EmptyBuilder;
+use Energine\share\gears\QAL;
+use Energine\share\gears\UserSession;
+
+class GoodsLastSeenList extends DataSet {
+
+    public function __construct($name, $module, array $params = NULL) {
+        parent::__construct($name, $module, $params);
+    }
+
+    protected function mainState() {
+
+        UserSession::start(true);
+        if (!empty($_SESSION['last_seen_goods'])) {
+            $this->setBuilder($b = new ComponentProxyBuilder());
+            $params = [
+                'active' => false,
+                'state' => 'main',
+                'id'    => $_SESSION['last_seen_goods'],
+                'list_features' => 'any' // вывод всех фич товаров в списке
+            ];
+            $b->setComponent('products',
+                '\\Energine\\shop\\components\\GoodsList',
+                $params);
+            $toolbars = $this->createToolbar();
+            if (!empty($toolbars)) {
+                $this->addToolbar($toolbars);
+            }
+            $this->js = $this->buildJS();
+        } else {
+            $this->setBuilder(new EmptyBuilder());
+        }
+    }
+
+}
