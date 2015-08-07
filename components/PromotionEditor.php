4275cdb691a16645a85bc59510c3adf18e1eed43
Pavel Dubenko <d.pavka@gmail.com>
2015-08-07 16:22:24 +0300

    promotions

--- components/PromotionEditor.php
+++ components/PromotionEditor.php
@@ -1,74 +1,94 @@
 <?php
 
 namespace Energine\shop\components;
+
 use Energine\share\components\Grid, Energine\share\gears\FieldDescription, Energine\share\gears\Field;
 use Energine\share\gears\QAL;
 
 class PromotionEditor extends Grid {
 
-	/**
-	 * @var PromotionGoodsEditor $oEditor
-	 */
-	private $oEditor;
+    /**
+     * @var PromotionGoodsEditor $oEditor
+     */
+    private $oEditor;
 
-	public function __construct($name,  array $params = null) {
-		parent::__construct($name, $params);
+    public function __construct($name, array $params = NULL) {
+        parent::__construct($name, $params);
         $this->setTableName('shop_promotions');
-	}
+    }
 
-	protected function prepare() {
+    /**
+     * @copydoc DBDataSet::getFKData
+     */
+    protected function getFKData($fkTableName, $fkKeyName) {
+        if (in_array($this->getState(), ['add', 'edit'])) {
+            if ($fkKeyName == 'site_id') {
+                $result =
+                    $this->dbh->getForeignKeyData($fkTableName, $fkKeyName, $this->document->getLang(), [$fkTableName.'.site_id' => E()->getSiteManager()->getSitesByTag('shop', true)]);
+            } else {
+                $result = parent::getFKData($fkTableName, $fkKeyName);
+            }
+        } else {
+            $result = parent::getFKData($fkTableName, $fkKeyName);
+        }
 
-		parent::prepare();
+        return $result;
+    }
 
-		if (in_array($this->getState(), array('add', 'edit'))) {
 
-			$fd = new FieldDescription('goods');
-			$fd->setType(FieldDescription::FIELD_TYPE_TAB);
-			$fd->setProperty('title', $this->translate('TAB_PROMOTION_GOODS'));
-			$this->getDataDescription()->addFieldDescription($fd);
+    protected function prepare() {
 
-			$field = new Field('goods');
-			$state = $this->getState();
-			$tab_url = (($state != 'add') ? $this->getData()->getFieldByName($this->getPK())->getRowData(0) : '') . '/goods/';
+        parent::prepare();
 
-			$field->setData($tab_url, true);
-			$this->getData()->addField($field);
-		}
-	}
+        if (in_array($this->getState(), ['add', 'edit'])) {
 
-	protected function goodsEditor() {
-		$sp = $this->getStateParams(true);
-		$params = array('config' => 'core/modules/shop/config/PromotionGoodsEditor.component.xml');
+            $fd = new FieldDescription('goods');
+            $fd->setType(FieldDescription::FIELD_TYPE_TAB);
+            $fd->setProperty('title', $this->translate('TAB_PROMOTION_GOODS'));
+            $this->getDataDescription()->addFieldDescription($fd);
 
-		if (isset($sp['promotion_id'])) {
-			$this->request->shiftPath(2);
-			$params['promotionID'] = $sp['promotion_id'];
+            $field = new Field('goods');
+            $state = $this->getState();
+            $tab_url = (($state != 'add') ? $this->getData()->getFieldByName($this->getPK())->getRowData(0) : '') . '/goods/';
 
-		} else {
-			$this->request->shiftPath(1);
-		}
-		$this->oEditor = $this->document->componentManager->createComponent('oEditor', 'Energine\shop\components\PromotionGoodsEditor', $params);
-		$this->oEditor->run();
-	}
+            $field->setData($tab_url, true);
+            $this->getData()->addField($field);
+        }
+    }
 
-	public function build() {
-		if ($this->getState() == 'goodsEditor') {
-			$result = $this->oEditor->build();
-		} else {
-			$result = parent::build();
-		}
+    protected function goodsEditor() {
+        $sp = $this->getStateParams(true);
+        $params = ['config' => 'core/modules/shop/config/PromotionGoodsEditor.component.xml'];
 
-		return $result;
-	}
+        if (isset($sp['promotion_id'])) {
+            $this->request->shiftPath(2);
+            $params['promotionID'] = $sp['promotion_id'];
 
-	protected function saveData() {
-		$promotionID = parent::saveData();
-		$this->dbh->modify(
-			'UPDATE shop_goods2promotions
+        } else {
+            $this->request->shiftPath(1);
+        }
+        $this->oEditor = $this->document->componentManager->createComponent('oEditor', 'Energine\shop\components\PromotionGoodsEditor', $params);
+        $this->oEditor->run();
+    }
+
+    public function build() {
+        if ($this->getState() == 'goodsEditor') {
+            $result = $this->oEditor->build();
+        } else {
+            $result = parent::build();
+        }
+
+        return $result;
+    }
+
+    protected function saveData() {
+        $promotionID = parent::saveData();
+        $this->dbh->modify(
+            'UPDATE shop_goods2promotions
 			SET session_id = NULL, promotion_id=%s
 			WHERE (promotion_id IS NULL and session_id = %s) or (promotion_id = %1$s)',
-			$promotionID, session_id()
-		);
-		return $promotionID;
-	}
+            $promotionID, session_id()
+        );
+        return $promotionID;
+    }
 }
